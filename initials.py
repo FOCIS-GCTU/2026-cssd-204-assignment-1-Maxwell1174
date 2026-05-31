b50a8f39be8469118a76c1c9b707c3a42a43acb6
Maxwell1174 <2425401174@live.gctu.edu.gh>
2026-05-31 17:40:04 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,8 +1,18 @@
+#File: initials.py
+#Description: Prints my initials in large size
+# #Assignment Number: 2
+# 
+# Name: Maxwell Ansong Anim
+# Student id: 2425401174
+# Email: 2425401174@live.
+# Gender: male
+#  
 
 def main():
-  pass #cprint()
-print("...MAA")
-print()
+
+    print()
+   
+
 print("...MMMM....MMMM.........AAAAA..........AAAAA.....")
 print("...MMMM....MMMM........AAAAAAA........AAAAAAA....")
 print("...MM.MM..MM.MM.......AA.....AA......AA.....AA...")
@@ -13,8 +23,7 @@ print("...MM..MMMM..MM......AA.......AA....AA.......AA..")
 print("...MM...MM...MM......AA.......AA....AA.......AA..")
 print("...MM...MM...MM..**..AA.......AA..**AA.......AA**")
 print("...MM...MM...MM..**..AA.......AA..**AA.......AA**")
-print()ode goes here
 
-
+print()
 
 main()
